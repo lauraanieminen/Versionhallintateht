d126049d3a9381a04a512b2700a66cf4c071c9af
lauraanieminen <nieminenla@gmail.com>
2024-10-20 10:48:10 +0300

    Valmis

---
+++ Versionhallintateht oikea!/src/App.java
@@ -0,0 +1,40 @@
+public class App {
+    public static void main(String[] args) {
+        
+      
+
+        String tekija = "Laura";
+        double luku1 = 5; 
+        double luku2 = 2; 
+        double tulo = luku1 * luku2; 
+        double erotus = luku1 - luku2; 
+        double summa = luku1 + luku2; 
+        double jako = luku1 / luku2; 
+
+
+        System.out.println("Hei! Olen tulostin ohjelma.");
+
+        System.out.print("Ohjelman tekijä: ");
+        
+        System.out.println(tekija); 
+
+       
+        System.out.println("Luku1-muuttujan arvo on " + luku1); 
+
+        
+        System.out.println("Luku2-muuttujan arvo on " + luku2); 
+
+        
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo);  
+
+       
+        System.out.println(luku1 + " - " + luku2 + " = " + erotus); 
+
+        
+        System.out.println(luku1 + " + " + luku2 + " = " + summa); 
+
+        
+        System.out.println(luku1 + " / " + luku2 + " = " + jako); 
+    }
+}
+
